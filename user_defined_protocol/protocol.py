fb36a2ef6ebdb84fc954186e5f77fa12c51cfdbe
younghyen7956 <155882166+younghyen7956@users.noreply.github.com>
2024-08-27 18:41:00 +0900

    feat: [김용현] socket server와 통신 [PROJ_AI_CLIENT-1]

--- user_defined_protocol/protocol.py
+++ user_defined_protocol/protocol.py
@@ -2,11 +2,10 @@ from enum import Enum
 
 
 class UserDefinedProtocolNumber(Enum):
-    # 예약된 정보 (1, 2, 11, 12, 13, 21) 을 제외하고 사용하도록 함
-    TCP_TEAM_LLAMA_TEST = 3 # 새로추가한 부분
+    # 예약된 정보 (1, 2, 4, 11, 12, 13, 21) 을 제외하고 사용하도록 함
+    GTP_OPENAI_TEST = 7
 
     FIRST_USER_DEFINED_FUNCTION_FOR_TEST = 5
-
 
     @classmethod
     def hasValue(cls, value):
